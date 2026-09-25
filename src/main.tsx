9d2bc9574264980f26a0f09cbb1921f76dab2cd7
Builder Of Things <gryszzz6@gmail.com>
2026-09-25 14:50:45 -0400

    tighten: reconcile src/main.tsx with Worldstate engine

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,7 @@
-import { StrictMode, lazy, Suspense } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import WorldShell from './world/WorldShell'
-
-const LegacyApp = lazy(() => import('./App.tsx'))
-
-function Root() {
-  const useLegacy = new URLSearchParams(window.location.search).get('legacy') === '1'
-
-  if (useLegacy) {
-    return (
-      <Suspense fallback={<div style={{ padding: 24 }}>Loading Atlasz legacy interface…</div>}>
-        <LegacyApp />
-      </Suspense>
-    )
-  }
-
-  return <WorldShell />
-}
+import Root from './Root'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
